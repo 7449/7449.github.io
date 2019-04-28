23fc1db4787a835891d7d300e5cf6538585f182b
7449 <yuezhaoyang7449@163.com>
2019-04-28 15:42:41 +0800

    delete app project

--- BlogApp/app/src/main/java/com/blog/net/Net.kt
+++
@@ -1,8 +0,0 @@
-package com.blog.net
-
-/**
- *  by y on 2017/9/14.
- */
-object Net {
-    const val BASE_URL: String = "https://7449.github.io/"
-}
--- BlogApp/app/src/main/java/com/blog/net/NetModel.kt
+++
@@ -1,6 +0,0 @@
-package com.blog.net
-
-/**
- *  by y on 2017/9/14.
- */
-class NetModel(var title: String, var littleTitle: String, val detailUrl: String)
